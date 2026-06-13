aae36acd30be408af870fd8c6b23c7f3a958ed15
Dmytro <poliak23444@gmail.com>
2026-06-13 19:54:03 +0300

    Phase 2.3: signup with autologin

--- personal_trein/urls.py
+++ personal_trein/urls.py
@@ -18,14 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from workouts.views import signup
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('exercises/', include('workouts.urls')),
+    path('accounts/signup/', signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
-
+    
 ]
 
 
--- workouts/views.py
+++ workouts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Exercise  
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 # Create your views here.
 
 def exercise_list(request):
@@ -25,3 +28,18 @@ def exercise_detail(request, pk):
     exercise = get_object_or_404(Exercise, pk=pk)
 
     return render (request, 'workouts/exercise_detail.html', {'exercise': exercise})
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('workouts:exercise_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+    
